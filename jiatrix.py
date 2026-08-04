abefdcf043d06d1a81d9c7ed22fcf10a3a708794
pyjoek <joelnrlson@gmail.com>
2026-08-04 16:30:35 +0300

    ai ready for use

---
+++ jiatrix.py
@@ -0,0 +1,8 @@
+from brain import ask
+
+while True:
+    user = input("[+] user: ")
+    if user.lower().strip() not in ["exit", "die", "goodbye", "off", "terminal"]:
+        ask(user)
+    else:
+        break
